dea07a8b132ffa419057ab54f875a513d9991457
soukaynabensalah <146835722+soukaynabensalah@users.noreply.github.com>
2023-11-17 08:50:59 +0100

    Create Class2.py

---
+++ Class2.py
@@ -0,0 +1,12 @@
+class car()
+    def _init_(self, name, speed, model, color, engine_power, nitro_speed ) :
+        self.name = name
+        self.speed = speed
+        self.model = model
+        self.enegine_power = enegine_power
+        self.color = color
+        self.nitro_speed = nitro_speed
+
+    def info(self) : 
+        print("name : {},speed : {} , model : {}") 
+  
